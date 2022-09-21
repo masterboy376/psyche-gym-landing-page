2d990d6168722a204abba88bd4382219f3bfeef3
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-09-21 14:22:40 +0000

    exeything done but navbar and real data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MembershipCard from './components/MembershipCard';
 
 function App() {
   return (
-    <div className={`text-gray-400 bg-slate-900 overflow-hidden`}>
+    <div className={`text-gray-400 bg-zinc-900 overflow-hidden`}>
       {/* header */}
       <header id='home' className={`relative w-full h-screen header-section`}>
         <section className={`container z-10 relative flex h-full mx-auto sm:flex-row sm:justify-between sm:items-center flex-col justify-end items-start`}>
@@ -25,28 +25,28 @@ function App() {
 
       {/* programs */}
       <section id='programs' className="container px-5 py-24 mx-auto">
-      <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="text-2xl font-medium title-font mb-4 text-white tracking-widest">TRAINING PROGRAMS</h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
-          </div>
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="text-2xl font-medium title-font mb-4 text-white tracking-widest">TRAINING PROGRAMS</h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+        </div>
         <div className='overflow-x-auto'>
-          <div className='flex sm:justify-evenly justify-start overflow-x-auto w-max mx-auto'>
-            <ProgramCard imgUrl={'abc'} title={'Weight Loss'} body={''} />
-            <ProgramCard imgUrl={'abc'} title={'Muscle Gain'} body={''} />
-            <ProgramCard imgUrl={'abc'} title={'Cardio'} body={''} />
-            <ProgramCard imgUrl={'abc'} title={'Weight Ligting'} body={''} />
+          <div className='flex sm:justify-evenly justify-start overflow-x-auto w-max mx-auto scroll-hidden'>
+            <ProgramCard imgUrl={'abc'} title={'Weight Loss'} body={'dgslg gfgldg gldfgldfgdf gdgldf gldfg dfgkdg dlgdf gdkg dlfgk'} />
+            <ProgramCard imgUrl={'abc'} title={'Muscle Gain'} body={'dgslg gfgldg gldfgldfgdf gdgldf gldfg dfgkdg dlgdf gdkg dlfgk'} />
+            <ProgramCard imgUrl={'abc'} title={'Cardio'} body={'dgslg gfgldg gldfgldfgdf gdgldf gldfg dfgkdg dlgdf gdkg dlfgk'} />
+            <ProgramCard imgUrl={'abc'} title={'Weight Ligting'} body={'dgslg gfgldg gldfgldfgdf gdgldf gldfg dfgkdg dlgdf gdkg dlfgk'} />
           </div>
         </div>
       </section>
 
       {/* memberships */}
       <section id='memberships' className="container px-5 py-24 mx-auto">
-      <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="text-2xl font-medium title-font mb-4 text-white tracking-widest">GYM MEMBERSHIP</h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
-          </div>
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="text-2xl font-medium title-font mb-4 text-white tracking-widest">GYM MEMBERSHIP</h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+        </div>
         <div className='overflow-x-auto'>
-          <div className='flex justify-start w-max mx-auto'>
+          <div className='flex justify-start w-max mx-auto py-4 scroll-hidden'>
             <MembershipCard price={999.00} center={false} title={'Starter'} details={[['Program flexibility.', true], ['Complete access to all the equipments for workout.', true], ['10 hrs / week maximum time limit,', true], ['Diet and suppliment support.', false], ['Personalised trainer.', false], ['Access to various gyming competition.', false], ['Injury insurance.', false]]} />
             <MembershipCard price={1999.00} center={true} title={'Advanced'} details={[['Program flexibility.', true], ['Complete access to all the equipments for workout.', true], ['21 hrs / week maximum time limit,', true], ['Diet and suppliment support.', true], ['Personalised trainer.', false], ['Access to various gyming competition.', false], ['Injury insurance.', false]]} />
             <MembershipCard price={4999.00} center={false} title={'Professional'} details={[['Program flexibility.', true], ['Complete access to all the equipments for workout.', true], ['No maximum time limit,', true], ['Diet and suppliment support.', true], ['Personalised trainer.', true], ['Access to various gyming competition.', true], ['Injury insurance.', true]]} />
@@ -55,7 +55,7 @@ function App() {
       </section>
 
       {/* gallary */}
-      <section className="body-font">
+      <section  id='gallery' className="body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
             <h1 className="text-2xl font-medium title-font mb-4 text-white tracking-widest">IMAGE GALLERY</h1>
@@ -90,7 +90,7 @@ function App() {
       </section>
 
       {/* trainers */}
-      <section className="body-font">
+      <section id='trainers' className="body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
             <h1 className="text-2xl font-medium title-font mb-4 text-white tracking-widest">OUR TRAINERS</h1>
@@ -142,27 +142,29 @@ function App() {
       </section>
 
       {/* contact */}
-      <section className="text-gray-600 body-font relative container mx-auto">
-  <div className="absolute rounded-lg inset-0 bg-gray-300">
-    <iframe width="100%" height="100%" frameBorder="0" marginHeight="0" marginWidth="0" title="map" scrolling="no" src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" style={{"filter": "grayscale(1) contrast(1.2) opacity(0.4)"}}></iframe>
-  </div>
-  <div className="container px-5 py-24 mx-auto flex">
-    <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-      <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-      <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
-      <div className="relative mb-4">
-        <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-        <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
-        <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-      </div>
-      <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-      <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
-    </div>
-  </div>
-</section>
+      <section id='contact' className="body-font relative container mx-auto">
+        <div className="absolute rounded-lg inset-0 bg-zinc-900">
+        <iframe width="100%" height="100%" frameBorder="0" marginHeight="0" marginWidth="0" title="map" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3444.4903869726627!2d78.03977152600993!3d30.308566302368412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3909292d93feb6c5%3A0x89eb2a29fad8ff!2sKhalsa%20Cricket%20Academy%20Dehradun!5e0!3m2!1sen!2sin!4v1663769603868!5m2!1sen!2sin" className='rounded-lg' style={{filter: 'grayscale(1) contrast(1.2) opacity(0.2)'}}></iframe>
+        </div>
+        <div className="container px-5 py-24 mx-auto flex">
+          <div className="lg:w-1/3 md:w-1/2 bg-zinc-900 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+            <h2 className="text-lg mb-1 font-medium title-font">Contact</h2>
+            <p className="leading-relaxed mb-5">+91 6893099021</p>
+            <p className="leading-relaxed mb-5">pankajsharma5491922@gmail.com</p>
+            <p className="leading-relaxed mb-5">Tyagi Road, Race Course, Dehradun, Uttarakhand, India, 248001</p>
+            <div className="relative mb-4">
+              <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+              <input type="email" id="email" name="email" className="w-full bg-zinc-800 rounded-lg border border-zinc-800 focus:border-orange-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-500 ease-in-out" />
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
+              <textarea id="message" name="message" className="w-full bg-zinc-800 rounded-lg border border-zinc-800 focus:border-orange-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-500 ease-in-out"></textarea>
+            </div>
+            <button className="hover:text-black border-2 transition-all duration-500 border-orange-500 py-2 px-6 focus:outline-none hover:bg-orange-500 rounded-lg text-lg">Send</button>
+            <p className="text-xs text-gray-500 mt-3">Your queries will be answered with full responsibility.</p>
+          </div>
+        </div>
+      </section>
 
       {/* footer */}
       <footer>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,18 @@ import {BsFacebook, BsTwitter, BsInstagram} from 'react-icons/bs';
 const Footer = () => {
   return (
     <div className='w-full px-4 py-2 mx-auto container flex sm:flex-row sm:justify-between sm:items-center flex-col justify-center items-start'>
-        <div className="flex sm:mx-0 mx-auto">
-            <span className="text-sm sm:text-base cursor-pointer sm:mx-4 mx-1 hover:text-orange-500">Home</span>
-            <span className="text-sm sm:text-base cursor-pointer sm:mx-4 mx-1 hover:text-orange-500">Programs</span>
-            <span className="text-sm sm:text-base cursor-pointer sm:mx-4 mx-1 hover:text-orange-500">Memberships</span>
-            <span className="text-sm sm:text-base cursor-pointer sm:mx-4 mx-1 hover:text-orange-500">Gallery</span>
-            <span className="text-sm sm:text-base cursor-pointer sm:mx-4 mx-1 hover:text-orange-500">Trainers</span>
+        <div className="grid sm:grid-rows-1 grid-rows-2 grid-flow-col gap-4 sm:mx-0 mx-auto">
+            <a href={'#home'} className="text-center transition-all duration-500 text-sm sm:text-base cursor-pointer sm:mx-4 mx-1 hover:text-orange-500">Home</a>
+            <a href={'#programs'} className="text-center transition-all duration-500 text-sm sm:text-base cursor-pointer sm:mx-4 mx-1 hover:text-orange-500">Programs</a>
+            <a href={'#memberships'} className="text-center transition-all duration-500 text-sm sm:text-base cursor-pointer sm:mx-4 mx-1 hover:text-orange-500">Memberships</a>
+            <a href={'#gallery'} className="text-center transition-all duration-500 text-sm sm:text-base cursor-pointer sm:mx-4 mx-1 hover:text-orange-500">Gallery</a>
+            <a href={'#trainers'} className="text-center transition-all duration-500 text-sm sm:text-base cursor-pointer sm:mx-4 mx-1 hover:text-orange-500">Trainers</a>
+            <a href={'#contact'} className="text-center transition-all duration-500 text-sm sm:text-base cursor-pointer sm:mx-4 mx-1 hover:text-orange-500">Contact</a>
         </div>
         <div className="flex sm:mx-0 mx-auto sm:mt-0 mt-2">
-            <span className="text-base cursor-pointer sm:mx-4 ml-2 hover:text-orange-500"><BsFacebook size={28}/></span>
-            <span className="text-base cursor-pointer sm:mx-4 ml-2 hover:text-orange-500"><BsTwitter size={28}/></span>
-            <span className="text-base cursor-pointer sm:mx-4 ml-2 hover:text-orange-500"><BsInstagram size={28}/></span>
+            <a className="transition-all duration-500 text-base cursor-pointer sm:mx-4 ml-2 hover:text-orange-500"><BsFacebook size={28}/></a>
+            <a className="transition-all duration-500 text-base cursor-pointer sm:mx-4 ml-2 hover:text-orange-500"><BsTwitter size={28}/></a>
+            <a className="transition-all duration-500 text-base cursor-pointer sm:mx-4 ml-2 hover:text-orange-500"><BsInstagram size={28}/></a>
         </div>
     </div>
   )
