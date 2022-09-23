0bba2bdf021dff3122bfb3210a6237e3846368fd
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2022-09-23 14:02:39 +0000

    fixed some errors

--- src/components/ProgramCard.js
+++ src/components/ProgramCard.js
@@ -6,8 +6,8 @@ const ProgramCard = ({imgUrl, title, body}) => {
   return (
     <div onMouseOver={()=>{setHover(true)}} onMouseOut={()=>{setHover(false)}} style={{"minWidth": "350px", "maxWidth": "350px"}} className={`rounded-lg border-2 border-orange-500 mx-4 p-2`}>
       <div className={`w-full h-56 rounded-lg relative`}>
-        <img src={imgUrl} className='w-full h-full rounded-lg absolute top-0 left-0' />
-        <div className={`w-full h-full rounded-lg bg-black absolute bottom-0 left-0 p-2 ${hover?'opacity-60':'opacity-0'} transition-all duration-500`}>{body}</div>
+        <img alt='' src={imgUrl} className='w-full h-full rounded-lg absolute top-0 left-0' />
+        <div className={`w-full h-full rounded-lg bg-black absolute bottom-0 left-0 p-2 ${hover?'opacity-70':'opacity-0'} transition-all duration-500`}>{body}</div>
       </div>
       <p className="text-center text-xl font-thin">{title}</p>
     </div>
